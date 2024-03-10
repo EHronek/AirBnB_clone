b4d6756a491e66da52a95ca82b26fac5e6399f39
EHronek <emmanuelhronek@gmail.com>
2024-03-10 12:37:19 +0000

    updated console.py with create function

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 """ Defines a class that contains the entry point of the command
     of the command interpreter"""
 from cmd import Cmd
-from models import BaseModel
+from models.base_model import BaseModel
 
 
 class HBNBCommand(Cmd):
@@ -22,10 +22,20 @@ class HBNBCommand(Cmd):
         """ Do nothing on empty input line"""
         pass
 
-    def do_create(self):
+    def do_create(self, arg):
         """ Creates a new instance of BaseModel, saves it to JSON
             and prints the `id` """
-        
+        try:
+            if not arg:
+                print('** class name missing **')
+            elif arg not in globals():
+                print("** class doesn't exist **")
+            else:
+                obj = BaseModel()
+                obj.save()
+                print(obj.id)
+        except Exception:
+            pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
