ad6971aa831d775cb36356ea401ed27fa6380f28
EHronek <emmanuelhronek@gmail.com>
2024-03-10 10:13:52 +0000

    commiting

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """ Defines a class that contains the entry point of the command
     of the command interpreter"""
 from cmd import Cmd
+from models import BaseModel
 
 
 class HBNBCommand(Cmd):
@@ -20,6 +21,11 @@ class HBNBCommand(Cmd):
     def emptyline(self):
         """ Do nothing on empty input line"""
         pass
+
+    def do_create(self):
+        """ Creates a new instance of BaseModel, saves it to JSON
+            and prints the `id` """
+        
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
