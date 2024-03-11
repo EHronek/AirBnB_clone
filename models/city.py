a9f9fc5c47e8048852db5c19357bff952d77ff67
EHronek <emmanuelhronek@gmail.com>
2024-03-11 09:19:44 +0000

    9-more classes

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Defines the City class """
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """ Defination of the City """
+    state_id = ''
+    name = ''
+
+    def __init__(self, *args, **kwargs)
+        """ initialization of the city """
+        super().__init__(*args, **kwargs)
---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Defines a class State that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Defination of State"""
+    name = ''
+
+    def __init__(self, args, **kwargs):
+        """initialization of State """
+        super().__init__(*args, **kwargs)
