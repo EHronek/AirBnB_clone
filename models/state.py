6fdac87e40a0eebd5b31f684ba55738194859fdc
EHronek <emmanuelhronek@gmail.com>
2024-07-07 18:07:30 +0000

    more models

--- models/state.py
+++ models/state.py
@@ -6,6 +6,7 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ Defination of State"""
     name = ''
+    state_id = ''
 
     def __init__(self, args, **kwargs):
         """initialization of State """
